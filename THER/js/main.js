ec8fe437db0187d46404cc6cb6e15c0773f0fb3b
thomaswoods86 <65785785+thomaswoods86@users.noreply.github.com>
2021-06-03 19:08:39 -0400

    Add files via upload

---
+++ THER/js/main.js
@@ -0,0 +1,20 @@
+
+$("document").ready(function () {
+
+$(".hamburger").click(function () {
+  $('.hamburger').toggleClass('is-active');
+  $(".overlay").fadeToggle();
+});
+
+var slider = tns({
+  container: ".launchpad__wrap",
+  loop: false,
+  edgePadding: 0,
+  mouseDrag: true,
+});
+
+$(".overlay .links a").click(function () {
+  $('.hamburger').toggleClass('is-active');
+  $(".overlay").hide();
+});
+});
